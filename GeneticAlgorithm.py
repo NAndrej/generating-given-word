73037ba2c4602734bf54d9259e7ef2381a44700b
Andrej Naumovski <smo.okmvm@gmail.com>
2019-07-25 12:30:37 +0200

    reformat

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -6,12 +6,13 @@ import string
 LIGHTBLUE = (30, 144, 255)
 WHITE = (255, 255, 255)
 
+
 class Population:
 
-    def __init__(self,pop_size,mutation_rate):
-        self.agents=[]
-        self.pop_size=pop_size
-        self.mutation_rate=mutation_rate
+    def __init__(self, pop_size, mutation_rate):
+        self.agents = []
+        self.pop_size = pop_size
+        self.mutation_rate = mutation_rate
         for i in range(pop_size):
             self.agents.append(Individual(target))
 
@@ -25,37 +26,37 @@ class Population:
 
     def selection(self):
         self.agents = sorted(self.agents, key=lambda agent: agent.fitness, reverse=True)
-        return self.agents[:int(0.2*self.pop_size)]
+        return self.agents[:int(0.2 * self.pop_size)]
 
-    def generate_new_population(self,selectedParents):
+    def generate_new_population(self, selectedParents):
         offSpring = []
 
-        for i in range(int((self.pop_size-len(selectedParents))/2)):
-            parent1=random.choice(selectedParents)
-            parent2=random.choice(selectedParents)
-            midPoint=len(parent1.string)/2
-            child1=Individual(parent1.target)
-            child2=Individual(parent2.target)
-            temp1=[]
-            temp2=[]
+        for i in range(int((self.pop_size - len(selectedParents)) / 2)):
+            parent1 = random.choice(selectedParents)
+            parent2 = random.choice(selectedParents)
+            midPoint = len(parent1.string) / 2
+            child1 = Individual(parent1.target)
+            child2 = Individual(parent2.target)
+            temp1 = []
+            temp2 = []
             for i in range(len(parent1.string)):
-                if i<midPoint:
+                if i < midPoint:
                     temp1.append(parent1.string[i])
                     temp2.append(parent2.string[i])
                 else:
                     temp1.append(parent2.string[i])
                     temp2.append(parent1.string[i])
 
-            child1.string=''.join(temp1)
-            child2.string=''.join(temp2)
-            self.mutate(child1,self.mutation_rate)
-            self.mutate(child2,self.mutation_rate)
+            child1.string = ''.join(temp1)
+            child2.string = ''.join(temp2)
+            self.mutate(child1, self.mutation_rate)
+            self.mutate(child2, self.mutation_rate)
             offSpring.append(child1)
             offSpring.append(child2)
-        newGen=selectedParents+offSpring
-        self.agents=newGen
+        newGen = selectedParents + offSpring
+        self.agents = newGen
 
-    def mutate(self,child,mutation_rate):
+    def mutate(self, child, mutation_rate):
         temp = []
         for i in range(len(child.string)):
             if (random.randint(0, 1) <= mutation_rate):
@@ -68,22 +69,24 @@ class Population:
 
 class Individual:
 
-    def __init__(self,target):
-        self.target=target
+    def __init__(self, target):
+        self.target = target
         self.string = ''.join(random.choice(string.ascii_letters) for x in range(len(self.target)))
         self.fitness = 0
+
     def toString(self):
         temp = "Word: " + self.string + ", Fitness: " + str(self.fitness)
         return temp
+
     def calculateFitness(self):
-        i=0
-        score=0
+        i = 0
+        score = 0
         for letter in self.string:
             if letter == self.target[i]:
-                score+=1
-            i+=1
+                score += 1
+            i += 1
 
-        self.fitness=int(score/len(self.target)*100)
+        self.fitness = int(score / len(self.target) * 100)
 
 
 if __name__ == "__main__":
@@ -93,17 +96,16 @@ if __name__ == "__main__":
     target = "stratocaster"
     generations = 5000
 
-    population=Population(pop_size,mutation_rate)
-    finished=False
-
+    population = Population(pop_size, mutation_rate)
+    finished = False
 
     for i in range(generations):
 
         population.calculateFitness()
-        selectedParents=population.selection()
+        selectedParents = population.selection()
         population.generate_new_population(selectedParents)
 
-        print(": Generation:",i,population.agents[0].toString())
+        print(": Generation:", i, population.agents[0].toString())
         # population.print()
         if any(agent.fitness == 100 for agent in population.agents):
             finished = True
@@ -111,7 +113,3 @@ if __name__ == "__main__":
 
     if finished:
         print(population.agents[0].toString())
-
-
-
-
